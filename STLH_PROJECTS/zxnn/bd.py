1fdf08c80d75a45bd620f45788cc1d9420aa3126
stlh13 <stlh13@63c3e646-9fb8-11de-b755-fd640f38fa9d>
2011-01-29 15:04:35 +0000

    css changed

--- STLH_PROJECTS/zxnn/bd.py
+++ STLH_PROJECTS/zxnn/bd.py
@@ -60,20 +60,53 @@ class BindIdHandler(webapp.RequestHandler):
       new_web_user = User(guser = user, nickname = new_web_user_id)
       new_web_user.put()
       self.redirect('/dashboard/main.html')
-class AvatarHandler(webapp.RequestHandler)
+class AvatarHandler(webapp.RequestHandler):
   def get(self):
     user = users.get_current_user()
+    if user:
+      log_url = users.create_logout_url(self.request.host_url)
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+      return
+    template_values = {'user': user,
+      'log_url': log_url,
+      }
+    path = os.path.join(os.path.dirname(__file__), 'template/bd/avatar.html')
+    self.response.out.write(template.render(path, template_values))
+  def post(self):
+    avatar_img = self.request.get('avatar_img')
+    u = User()
+    u.avatar = db.Blob(u)
+    u.put();
+    user = users.get_current_user()
+    if user:
+      log_url = users.create_logout_url(self.request.host_url)
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+      return
     template_values = {'user': user,
       'log_url': log_url,
       }
     path = os.path.join(os.path.dirname(__file__), 'template/bd/avatar.html')
     self.response.out.write(template.render(path, template_values))
 
+class AvatarImageHander(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write('get user id')
+    #user = db.get(self.request.get('user_id')
+    #if user.avatar:
+    #  self.response.headers['Content-Type'] = 'image/png'
+    #  self.response.out.write(user.avatar)
+    #else:
+    #  self.response.out.write('no image found')
+
 def main():
   application = webapp.WSGIApplication([('/dashboard/main.html', DashboardMainHandler),
-  ('/dashboard/bind_id.html', BindIdHandler),
-  ],
-  debug=True)
+      ('/dashboard/bind_id.html', BindIdHandler),
+      ('/dashboard/avatar.html', AvatarHandler),
+      ('/public/\w+/avatar', AvatarImageHander),
+    ],
+    debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == '__main__':
