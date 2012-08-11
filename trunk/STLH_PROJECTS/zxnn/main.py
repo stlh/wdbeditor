9241f2f6d4f156eb0f180ac016dd292758141109
stlh13@gmail.com <stlh13@gmail.com@63c3e646-9fb8-11de-b755-fd640f38fa9d>
2012-08-11 14:02:19 +0000

    upgrate to pytho 2.7

--- trunk/STLH_PROJECTS/zxnn/main.py
+++ trunk/STLH_PROJECTS/zxnn/main.py
@@ -2,30 +2,29 @@
 
 import os
 
-import wsgiref.handlers
-
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
 from google.appengine.api import users
 
-class MainPageHandler(webapp.RequestHandler):
-  def get(self):
-    user = users.get_current_user()
-    if user:
-      log_url = users.create_logout_url(self.request.host_url)
-    else:
-      log_url = users.create_login_url(self.request.uri)
-    template_values = {'user': user,
-                       'log_url': log_url,}
-    path = os.path.join(os.path.dirname(__file__), 'template/main.html')
-    self.response.out.write(template.render(path, template_values))
+import webapp2
 
-def main():
-  application = webapp.WSGIApplication([('/', MainPageHandler),
-      ('/index\.html', MainPageHandler),
-    ],
-    debug=False)
-  wsgiref.handlers.CGIHandler().run(application)
+import jinja2
 
-if __name__ == '__main__':
-  main()
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainPageHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            log_url = users.create_logout_url(self.request.host_url)
+        else:
+             log_url = users.create_login_url(self.request.uri)
+        template_values = {
+           'user': user,
+           'log_url': log_url,
+        }
+        template = env.get_template('template/main.html')
+        self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPageHandler),
+                               ('/index\.html', MainPageHandler),
+                              ],
+                              debug=True)
