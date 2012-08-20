28bb6daa88991074da55eb6e3af8e00b28e7ff1b
stlh13@gmail.com <stlh13@gmail.com@63c3e646-9fb8-11de-b755-fd640f38fa9d>
2012-08-20 11:45:32 +0000

    add ZxUser, bind nickname to google user

--- trunk/STLH_PROJECTS/zxnn/main.py
+++ trunk/STLH_PROJECTS/zxnn/main.py
@@ -3,23 +3,29 @@
 import os
 
 from google.appengine.api import users
+from google.appengine.ext import db
 
 import webapp2
 
 import jinja2
+
+from ilog import iLogItem
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
+        nickname = None
         if user:
             log_url = users.create_logout_url(self.request.host_url)
+            nickname = user.nickname()
         else:
             log_url = users.create_login_url(self.request.uri)
         template_values = {
            'user': user,
            'log_url': log_url,
+           'nickname': nickname,
         }
         template = env.get_template('template/main.html')
         self.response.out.write(template.render(template_values))
