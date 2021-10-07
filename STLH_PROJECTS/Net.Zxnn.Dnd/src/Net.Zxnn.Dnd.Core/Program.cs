382cfb9fa558c7080e3f4c44d619ae504155b527
Skoll <stlh13@gmail.com>
2021-10-07 19:36:15 +0800

    add system.commandline

--- STLH_PROJECTS/Net.Zxnn.Dnd/src/Net.Zxnn.Dnd.Core/Program.cs
+++ STLH_PROJECTS/Net.Zxnn.Dnd/src/Net.Zxnn.Dnd.Core/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
 using Net.Zxnn.Dnd.Core;
 using Net.Zxnn.Dnd.Core.Class;
 using Net.Zxnn.Dnd.Core.Race;
@@ -8,55 +11,81 @@ namespace Net.Zxnn.Dnd.Core
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Character c001 = new CharacterBuilder()
-                .WithRace(DndRace.Human)
-                .WithClass(DndClass.Fighter)
-                .WithName("Skoll-001")
-                .Build();
+            Console.WriteLine("Hello World!");
 
-            Character c002 = new CharacterBuilder()
-                .WithRace(DndRace.Dwarf)
-                .WithClass(DndClass.Fighter)
-                .WithName("Skoll-002")
-                .Build();
-
-            c001.EquipmentSockets.Weapon = new Battleaxe();
-            c002.EquipmentSockets.Weapon = new Battleaxe();
-            c002.EquipmentSockets.Shield = new Shield();
-
-            c001.move();
-            c002.move();
-
-            int c001Win = 0;
-            int c002Win = 0;
-            for (int i = 0; i < 10000; ++i)
+            var rootCommand = new RootCommand
             {
-                while (c001.HitPoints > 0 && c002.HitPoints > 0) {
-                    c001.attack(c002);
-                    if (c002.HitPoints > 0) {
-                        c002.attack(c001);
-                    }
-                }
+                new Option<int>(
+                    "--round-count",
+                    getDefaultValue: () => 10000,
+                    description: "how many rounds to be fight"
+                )
+            };
 
-                if (c001.HitPoints > 0) {
-                    c001Win += 1;
-                }
-                else if (c002.HitPoints > 0) {
-                    c002Win += 1;
-                }
-                else
+            rootCommand.Description = "DnD test application";
+
+            rootCommand.Handler = CommandHandler.Create<int>((int roundCount) =>
+            {
+                Console.WriteLine($"round count: {roundCount}");
+
+                Character c001 = new CharacterBuilder()
+                    .WithRace(DndRace.Human)
+                    .WithClass(DndClass.Fighter)
+                    .WithName("Skoll-001")
+                    .Build();
+
+                Character c002 = new CharacterBuilder()
+                    .WithRace(DndRace.Dwarf)
+                    .WithClass(DndClass.Fighter)
+                    .WithName("Skoll-002")
+                    .Build();
+
+                c001.EquipmentSockets.Weapon = new Battleaxe();
+                c002.EquipmentSockets.Weapon = new Battleaxe();
+                c002.EquipmentSockets.Shield = new Shield();
+
+                c001.move();
+                c002.move();
+
+                int c001Win = 0;
+                int c002Win = 0;
+                for (int i = 0; i < roundCount; ++i)
                 {
-                    Console.WriteLine("who win?");
+                    while (c001.HitPoints > 0 && c002.HitPoints > 0)
+                    {
+                        c001.attack(c002);
+                        if (c002.HitPoints > 0)
+                        {
+                            c002.attack(c001);
+                        }
+                    }
+
+                    if (c001.HitPoints > 0)
+                    {
+                        c001Win += 1;
+                    }
+                    else if (c002.HitPoints > 0)
+                    {
+                        c002Win += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("who win?");
+                    }
+
+                    // reset hit points
+                    c001.HitPoints = c001.HitPointMax;
+                    c002.HitPoints = c002.HitPointMax;
                 }
 
-                // reset hit points
-                c001.HitPoints = c001.HitPointMax;
-                c002.HitPoints = c002.HitPointMax;
-            }
+                Console.WriteLine($"c001 win: {c001Win}, c002 win: {c002Win}");
 
-            Console.WriteLine($"c001 win: {c001Win}, c002 win: {c002Win}");
+                return 0;
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
         }
     }
 }
