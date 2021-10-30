0be8421d937b78918f9cc81bdabdd912df2d2042
Skoll <stlh13@gmail.com>
2021-10-30 22:36:12 +0800

    add DI and logging

--- STLH_PROJECTS/Net.Zxnn.Dnd/src/Net.Zxnn.Dnd.Core/Core/Scene.cs
+++ STLH_PROJECTS/Net.Zxnn.Dnd/src/Net.Zxnn.Dnd.Core/Core/Scene.cs
@@ -1,7 +1,86 @@
-using System;
+using Net.Zxnn.Dnd.Core;
+using Net.Zxnn.Dnd.Core.Class;
+using Net.Zxnn.Dnd.Core.Equipments.Weapons;
+using Net.Zxnn.Dnd.Core.Race;
+using Microsoft.Extensions.Logging;
 
-public class Scene {
-    public void Init() {
-        Console.WriteLine("scene init");
+namespace Net.Zxnn.Dnd.Core
+{
+    public class Scene
+    {
+        private readonly ILogger _logger;
+        private readonly CombatService _combatService;
+
+        public Scene(ILogger<Scene> logger, CombatService combatService)
+        {
+            _logger = logger;
+            _combatService = combatService;
+        }
+
+        public void Run(int roundCount)
+        {
+            _logger.LogInformation($"round count: {roundCount}");
+
+            int c001Win = 0;
+            int c002Win = 0;
+            for (int i = 0; i < roundCount; ++i)
+            {
+                Character c001 = new CharacterBuilder()
+                    .WithRace(DndRace.Human)
+                    .WithClass(DndClass.Fighter)
+                    .WithAbilities(AbilityTools.GetRandomStandardSetOfScores())
+                    .WithName("Human-Fighter")
+                    .Build();
+
+                Character c002 = new CharacterBuilder()
+                    .WithRace(DndRace.Dwarf)
+                    .WithClass(DndClass.Barbarian)
+                    .WithAbilities(AbilityTools.GetRandomStandardSetOfScores())
+                    .WithName("Dwarf-Barbarian")
+                    .Build();
+
+                c001.EquipmentSockets.Weapon = new Battleaxe();
+                c002.EquipmentSockets.Weapon = new Battleaxe();
+
+                while (c001.HitPoints > 0 && c002.HitPoints > 0)
+                {
+                    if (c001.AbilityScores.Dexterity >= c002.AbilityScores.Dexterity)
+                    {
+                        _combatService.attack(c001, c002);
+                        if (c002.HitPoints > 0)
+                        {
+                            _combatService.attack(c002, c001);
+                        }
+                    }
+                    else
+                    {
+                        _combatService.attack(c002, c001);
+                        if (c001.HitPoints > 0)
+                        {
+                            _combatService.attack(c001, c002);
+                        }
+                    }
+                }
+
+                if (c001.HitPoints > 0)
+                {
+                    c001Win += 1;
+                }
+                else if (c002.HitPoints > 0)
+                {
+                    c002Win += 1;
+                }
+                else
+                {
+                    _logger.LogDebug("who win?");
+                }
+
+                // reset hit points
+                c001.HitPoints = c001.HitPointMax;
+                c002.HitPoints = c002.HitPointMax;
+            }
+
+            _logger.LogInformation($"c001 win: {c001Win}, c002 win: {c002Win}");
+        }
     }
 }
--- STLH_PROJECTS/Net.Zxnn.Dnd/src/Net.Zxnn.Dnd.Core/Program.cs
+++ STLH_PROJECTS/Net.Zxnn.Dnd/src/Net.Zxnn.Dnd.Core/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Net.Zxnn.Dnd.Core;
-using Net.Zxnn.Dnd.Core.Class;
-using Net.Zxnn.Dnd.Core.Race;
-using Net.Zxnn.Dnd.Core.Equipments.Weapons;
 
-namespace Net.Zxnn.Dnd.Core
+namespace Net.Zxnn.Dnd
 {
     class Program
     {
@@ -26,78 +24,29 @@ namespace Net.Zxnn.Dnd.Core
 
             rootCommand.Description = "DnD test application";
 
-            rootCommand.Handler = CommandHandler.Create<int>((int roundCount) =>
-            {
-                Console.WriteLine($"round count: {roundCount}");
+            rootCommand.Handler = CommandHandler.Create<int>((int roundCount) => {
+                var services = new ServiceCollection();
 
-                Character c001 = new CharacterBuilder()
-                    .WithRace(DndRace.Human)
-                    .WithClass(DndClass.Fighter)
-                    .WithAbilities(AbilityTools.GetRandomStandardSetOfScores())
-                    .WithName("Human-Fighter")
-                    .Build();
+                ConfigureServices(services);
 
-                Character c002 = new CharacterBuilder()
-                    .WithRace(DndRace.Dwarf)
-                    .WithClass(DndClass.Barbarian)
-                    .WithAbilities(AbilityTools.GetRandomStandardSetOfScores())
-                    .WithName("Dwarf-Barbarian")
-                    .Build();
-
-                c001.EquipmentSockets.Weapon = new Battleaxe();
-                c002.EquipmentSockets.Weapon = new Battleaxe();
-                //c002.EquipmentSockets.Shield = new Shield();
-
-                c001.move();
-                c002.move();
-
-                int c001Win = 0;
-                int c002Win = 0;
-                for (int i = 0; i < roundCount; ++i)
+                using (ServiceProvider serviceProvider = services.BuildServiceProvider())
                 {
-                    while (c001.HitPoints > 0 && c002.HitPoints > 0)
-                    {
-                        if (i % 2 == 0) {
-                            c001.attack(c002);
-                            if (c002.HitPoints > 0)
-                            {
-                                c002.attack(c001);
-                            }
-                        }
-                        else
-                        {
-                            c002.attack(c001);
-                            if (c001.HitPoints > 0)
-                            {
-                                c001.attack(c002);
-                            }
-                        }
-                    }
-
-                    if (c001.HitPoints > 0)
-                    {
-                        c001Win += 1;
-                    }
-                    else if (c002.HitPoints > 0)
-                    {
-                        c002Win += 1;
-                    }
-                    else
-                    {
-                        Console.WriteLine("who win?");
-                    }
-
-                    // reset hit points
-                    c001.HitPoints = c001.HitPointMax;
-                    c002.HitPoints = c002.HitPointMax;
+                    Scene scene = serviceProvider.GetService<Scene>();
+                    
+                    scene.Run(roundCount);
                 }
-
-                Console.WriteLine($"c001 win: {c001Win}, c002 win: {c002Win}");
-
-                return 0;
             });
 
             return rootCommand.InvokeAsync(args).Result;
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(configure => 
+                    configure.AddFilter("Net.Zxnn.Dnd", LogLevel.Information)
+                        .AddConsole())
+                .AddTransient<Scene>()
+                .AddTransient<CombatService>();
         }
     }
 }
